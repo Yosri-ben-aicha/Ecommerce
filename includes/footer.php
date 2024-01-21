30887133beec0e3baa5c9be021726e263adc50ac
Yosri-ben-aicha <yosribenaicha1111gmail.com>
2024-01-21 20:30:05 +0100

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-center hidden-xs" style="a:40px">
+        <b>All rights reserved</b>
+      </div>
+      <strong style="a:40px">Copyright &copy; Created By Aziz Slimen></strong>
+    </div>
+</footer>
